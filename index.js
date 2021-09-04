9a661bfe6b7eff0245a6100ee7d456696ae2e866
Ninte Dangana <ninte.dev@gmail.com>
2021-09-04 08:15:32 +0000

    Completed telephone number format check using regex

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+// checks for full string match
+function matchExact(r, text) {
+  const match = text.match(r);
+  return match && text === match[0];
+}
+
+// core function
 function telephoneCheck(str) {
-  return true;
+
+  // regex check for number formats
+  const regexCheck = /1? ?(\([0-9]{3}\)|[0-9]{3}) ?-?[0-9]{3} ?-?[0-9]{4}/;
+
+  // return true if exact match, false otherwise
+  if ((matchExact(regexCheck, str))) {
+    return true;
+  } else {
+    return false;
+  }
+  
 }
 
 telephoneCheck("555-555-5555");
